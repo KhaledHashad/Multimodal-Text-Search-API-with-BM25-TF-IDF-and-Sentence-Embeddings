95917c7bc87383b60e024a91acc191a7ed1088d1
KhaledHashad <94147008+KhaledHashad@users.noreply.github.com>
2025-07-16 10:06:29 +0300

    Update app.py

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ from sklearn.feature_extraction.text import TfidfVectorizer
 from sentence_transformers import SentenceTransformer, util
 from pyngrok import ngrok, conf
 
-# 🔐 Set your ngrok Authtokenn
+# 🔐 Set your ngrok Authtoken
 conf.get_default().auth_token = "2zp8rTsJ1Gg6Fgf8GKNO7dY9YJ8_6vxC1e4TqKgPVee2XMe1a"
 
 # --- Create Flask App ---
